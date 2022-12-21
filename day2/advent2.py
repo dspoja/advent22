091499bc11c5ae5609188e07203b9d82e5facd91
Danja Spoja <9200064+dspoja@users.noreply.github.com>
2022-12-20 19:00:47 -0500

    Finish day 2, part 2

--- day2/advent2.py
+++ day2/advent2.py
@@ -10,6 +10,12 @@ class Game(Enum):
     Scissors = 3
 
 
+class Outcome(Enum):
+    Win = 6
+    Tie = 3
+    Loss = 0
+
+
 def determine_winner(p1: Game, p2: Game) -> int:
     # Rock beats Scissors
     # Scissors beat Paper
@@ -33,6 +39,24 @@ def determine_winner(p1: Game, p2: Game) -> int:
             return p2.value + 6
 
 
+def determine_outcome(p1: Game, outcome: Outcome) -> int:
+    if outcome == Outcome.Loss:
+        if p1 == Game.Rock:
+            return Game.Scissors.value + Outcome.Loss.value
+        if p1 == Game.Paper:
+            return Game.Rock.value + Outcome.Loss.value
+        if p1 == Game.Scissors:
+            return Game.Paper.value + Outcome.Loss.value
+    elif outcome == Outcome.Win:
+        if p1 == Game.Rock:
+            return Game.Paper.value + Outcome.Win.value
+        if p1 == Game.Paper:
+            return Game.Scissors.value + Outcome.Win.value
+        if p1 == Game.Scissors:
+            return Game.Rock.value + Outcome.Win.value
+    else:
+        return p1.value + Outcome.Tie.value
+
 def day2():
     print("###########")
     print("# Day  2  #")
@@ -45,11 +69,20 @@ def day2():
         "Y": Game.Paper,
         "Z": Game.Scissors
     }
-    score = 0
+    outcome_map = {
+        "X": Outcome.Loss,
+        "Y": Outcome.Tie,
+        "Z": Outcome.Win
+    }
+    part1_score = 0
+    part2_score = 0
     with open("day2/input2", "rb") as data:
         for line in data:
             line = line.strip().decode("utf8")
             player1 = game_map.get(line[0])
             player2 = game_map.get(line[2])
-            score += determine_winner(player1, player2)
-        print(f"Part 1: Game score is {score}")
+            part1_score += determine_winner(player1, player2)
+            outcome = outcome_map.get(line[2])
+            part2_score += determine_outcome(player1, outcome)
+        print(f"Part 1: Game score is {part1_score}")
+        print(f"Part 2: Game score is {part2_score}")
