2b35c9a1f2782abb12bee4cccf93933a53f12594
Danja Spoja <9200064+dspoja@users.noreply.github.com>
2023-01-31 18:31:04 -0500

    Part 2 for day 4

--- day4/advent4.py
+++ day4/advent4.py
@@ -8,6 +8,24 @@ def determine_containment(e1_1, e1_2, e2_1, e2_2) -> bool:
         return False
 
 
+
+'''
+24-98,24-77
+96-98,34-95
+29-54,50-53
+
+if 1-1 
+
+'''
+def determine_overlap(e1_1, e1_2, e2_1, e2_2) -> bool:
+    if e1_1 <= e2_2 and e1_2 >= e2_1:
+        print(f"{e1_1}-{e1_2} is overlapping with {e2_1}-{e2_2}")
+        return True
+    else:
+        print(f"{e1_1}-{e1_2} is NOT overlapping with {e2_1}-{e2_2}")
+        return False
+
+
 def day4():
     print("###########")
     print("# Day  4  #")
@@ -15,11 +33,18 @@ def day4():
 
     with open("day4/input4", "rb") as data:
         contain_count = 0
+        overlap_count = 0
         for line in data:
             line = line.strip().decode("utf8")
             data = list(map(int, re.split(',|-', line)))
+            # Part 1
             if determine_containment(data[0], data[1], data[2], data[3]):
                 contain_count += 1
             elif determine_containment(data[2], data[3], data[0], data[1]):
                 contain_count += 1
+            # Part 2
+            if determine_overlap(data[0], data[1], data[2], data[3]):
+                overlap_count += 1
+
         print(f"Part 1: There are {contain_count} contained pairs")
+        print(f"Part 2: There are {overlap_count} overlapped pairs")
